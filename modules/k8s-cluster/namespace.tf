1301ee49faa3ac9fc6c71b181218308191c9d524
Aram Karapetyan <aram@dasmeta.com>
2024-02-22 15:53:15 +0400

    feat(DMVP-3571): K8S cluster module

--- modules/k8s-cluster/namespace.tf
+++ modules/k8s-cluster/namespace.tf
@@ -2,14 +2,6 @@ resource "kubernetes_namespace" "namespace" {
   for_each = { for idx, name in var.namespaces : name => idx }
 
   metadata {
-    annotations = {
-      "linkerd.io/inject"                      = "disabled"
-      "config.linkerd.io/proxy-cpu-limit"      = "1"
-      "config.linkerd.io/proxy-cpu-request"    = "0.2"
-      "config.linkerd.io/proxy-memory-limit"   = "1Gi"
-      "config.linkerd.io/proxy-memory-request" = "128Mi"
-    }
-
     labels = {
       name = each.key
     }
